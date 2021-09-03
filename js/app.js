db4a921a9bdfed8e6d7f5c0e8557a974d10fed56
Md-Anisuzzman <Anisuzzaman1199@gmail.com>
2021-09-03 21:36:30 +0600

    2ndt commit

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ const searchBook = () => {
   
     /* Dynamic-Fetch-URL*/
   
-    const url = `http://openlibrary.org/search.json?q=${searchText}`
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
   
     fetch(url)
       .then(res => res.json())
